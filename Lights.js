07847b40076909c7b7ec1e8ffc87cb930e8b5cce
Fabian Schnekenburger <fabian.schnekenburger@hs-offenburg.de>
2021-01-22 11:42:58 +0100

    adapt for multiple ids but only one led

--- Lights.js
+++ Lights.js
@@ -8,13 +8,12 @@ class Lights {
     /**
      * Constructor
      * @param redGpioPort GPIO pin corresponding to the red LED
-     * @param greenGpioPort GPIO pin corresponding to the green LED
-     * @param blueGpioPort GPIO pin corresponding to the blue LED
      */
-    constructor(redGpioPort, greenGpioPort, blueGpioPort) {
-        this.redLight = new Gpio(redGpioPort, {mode: Gpio.OUTPUT});
-        this.greenLight = new Gpio(greenGpioPort, {mode: Gpio.OUTPUT});
-        this.blueLight = new Gpio(blueGpioPort, {mode: Gpio.OUTPUT});
+    constructor(ledPorts) {
+        this.leds = []
+        for(var port in ledPorts) {
+            this.leds.append(new Gpio(port.gpio), {mode: Gpio.OUTPUT})
+        }
         this.flashTimer = null;
         this.flashStateIsOn = false;
     }
@@ -23,19 +22,17 @@ class Lights {
      * Start flashing a given color at a given frequency. LED will continue to flash until
      * the program stops, {@link stopFlashing} is called, or one of the specific color methods are
      * called, including {@link off}. {@link pwmWrite} and {@link write} do NOT stop flashing.
-     * @param redPwm {number} PWM value to flash for the red LED
-     * @param greenPwm {number} PWM value to flash for the green LED
-     * @param bluePwm {number} PWM value to flash for the blue LED
+     * @param brightness PWM value to flash for the LED
      * @param frequency Frequency in milliseconds to flash the LEDs at.
      */
-    startFlashing(redPwm, greenPwm, bluePwm, frequency) {
+    startFlashing(brightness, frequency) {
         this.stopFlashing();
 
         this.flashTimer = setInterval(() => {
             if(this.flashStateIsOn) {
-                this.pwmWrite(0, 0, 0);
+                this.pwmWrite();
             } else {
-                this.pwmWrite(redPwm, greenPwm, bluePwm);
+                this.pwmWrite(brightness);
             }
 
             this.flashStateIsOn = !this.flashStateIsOn;
@@ -57,56 +54,26 @@ class Lights {
      */
     off() {
         this.stopFlashing();
-        this.write(false, false, false);
+        for( var led in this.leds ) {
+            led.digitalWrite(0);
+        }
     }
 
-    /**
-     * Turn on the red LED and turn off the blue and green LEDs
-     */
-    red() {
+    ledOn(index){
         this.stopFlashing();
-        this.write(true, false, false);
+        this.leds[index].digitalWrite(1); 
     }
 
-    /**
-     * Turn on the green LED and turn off the blue and red LEDs
-     */
-    green() {
+    ledOff(index){
         this.stopFlashing();
-        this.write(false, true, false);
+        this.leds[index].digitalWrite(0); 
     }
 
-    /**
-     * Turn on the red LED and green LED (at 27% brightness).
-     * This replicates a yellow-like color but necessary powers may vary between different LEDs.
-     */
-    yellow() {
-        this.stopFlashing();
-        this.pwmWrite(255, 70, 0);
-    }
-
-    /**
-     * Write an on/off state to all three LEDs.
-     * @param red {boolean} Whether the red LED should be turned on.
-     * @param green {boolean} Whether the green LED should be turned on.
-     * @param blue {boolean} Whether the blue LED should be turned on.
-     */
-    write(red = false, green = false, blue = false) {
-        this.redLight.digitalWrite(red ? 1 : 0);
-        this.greenLight.digitalWrite(green ? 1 : 0);
-        this.blueLight.digitalWrite(blue ? 1 : 0);
-    }
-
-    /**
-     * Write a PWM state to all three LEDs.
-     * @param red {number} Red LED PWM value. Expected to be between 0 and 255, inclusive.
-     * @param green {number} Green LED PWM value. Expected to be between 0 and 255, inclusive.
-     * @param blue {number} Blue LED PWM value. Expected to be between 0 and 255, inclusive.
-     */
-    pwmWrite(red, green, blue) {
-        this.redLight.pwmWrite(red);
-        this.greenLight.pwmWrite(green);
-        this.blueLight.pwmWrite(blue);
+    pwmWrite(value) {
+        for(var led in this.leds) {
+            led.pwmWrite(value);
+        }
+        
     }
 }
 
--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const config = require('./tally.config.json');
 const { Atem } = require('atem-connection');
 
 const switcher = new Atem();
-const lights = new Lights(config.ledGpioPins.red, config.ledGpioPins.green, config.ledGpioPins.blue);
+const lights = new Lights(config.leds);
 
 // Flash to indicate the tally is currently disconnected
-lights.startFlashing(config.disconnectedFlashColor.red, config.disconnectedFlashColor.green,
-	config.disconnectedFlashColor.blue, config.disconnectedFlashFrequency);
+lights.startFlashing(config.disconnectedFlashBrightness, config.disconnectedFlashFrequency);
 
 console.log("Connecting...");
 switcher.connect(config.switcherIP);
@@ -15,13 +14,13 @@ switcher.connect(config.switcherIP);
 switcher.on('connected', () => {
 	console.log("Connected.");
 	lights.stopFlashing();
+	lights.off();
 });
 
 switcher.on('disconnected', () => {
 	console.log("Lost connection!");
 	// Flash to indicate the tally is currently disconnected
-	lights.startFlashing(config.disconnectedFlashColor.red, config.disconnectedFlashColor.green,
-		config.disconnectedFlashColor.blue, config.disconnectedFlashFrequency);
+	lights.startFlashing(config.disconnectedFlashBrightness, config.disconnectedFlashFrequency); //
 });
 
 switcher.on('stateChanged', (state) => {
@@ -29,20 +28,21 @@ switcher.on('stateChanged', (state) => {
 	if(!state || !state.video || !state.video.ME || !state.video.ME[0])
 		return;
 
-	const preview = state.video.ME[0].previewInput;
+	// const preview = state.video.ME[0].previewInput;
 	const program = state.video.ME[0].programInput;
 
 	// If faded to black, lights are always off
 	if(state.video.ME[0].fadeToBlack && state.video.ME[0].fadeToBlack.isFullyBlack) {
 		lights.off();
 	// This camera is either in program OR preview, and there is an ongoing transition.
-	} else if(state.video.ME[0].inTransition && (program === config.inputID || preview === config.inputID)) {
-		lights.yellow();
-	} else if(program === config.inputID) {
-		lights.red();
-	} else if(preview === config.inputID) {
-		lights.green();
-	} else { // Camera is not in preview or program
-		lights.off();
+	} else{
+
+		for(var i=0; i<config.leds.length; i++) {}
+			if(program === config.leds[i].inputID) {
+				lights.ledOn(i); 
+			}else{
+				lights.off(i);
+			}
+		}
 	}
-});
+);
